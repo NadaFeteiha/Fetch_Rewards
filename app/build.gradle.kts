dec7d2e1b02f25865978863b4400af9496ab2954
Nada Feteiha <nada.feteiha@gmail.com>
2025-03-06 22:04:12 -0800

    Refactor MainActivity path, add Kotlin serialization plugin, and update strings for rewards

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-parcelize")
 }
 
 android {
@@ -57,4 +58,5 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation (libs.bundles.koin)
     implementation (libs.bundles.retrofit)
+
 }
